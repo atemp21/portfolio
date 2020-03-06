b91b82c3e09b130e34d5e01aab1cd454992e450b
atemp21 <aaron.templeton@live.com>
2020-03-05 17:06:36 -0700

    made some text changes and styling changes for better fit

--- work.js
+++ work.js
@@ -16,7 +16,7 @@ function get_json_data(){
     $.getJSON('./work.json', function(data){
         var i = 0;
         $.each(data, function(key, val){
-            $("#recent-work").append('<div class="work-card"><h3 class="work-header">'+val.title+'</h3><p class="work-desc">'+val.desc+'</p><div class="work-preview"><a href="'+val.url+'" target="_blank"><img src="'+val.photo+'" alt=""></a></div><ul id="tool-'+i+'" class="work-tools">');
+            $("#recent-work").append('<div class="work-card"><h3 class="work-header">'+val.title+'</h3><p class="work-desc">'+val.desc+'<br><a href="'+val.code+'">code</a><a href="'+val.url+'">project</a></p><div class="work-preview"><a href="'+val.url+'" target="_blank"><img src="'+val.photo+'" alt=""></a></div><ul id="tool-'+i+'" class="work-tools">');
             $.when($.each(val.lang, function(k,v){
                 $("#tool-"+i).append('<li>'+v+'</li>');
             })).done(function(){
